c58b262e8d34b6e61057065f7ab067a00da70293
Winnie <63833544+Today-Asked@users.noreply.github.com>
2022-06-20 16:43:13 +0800

    update

--- GameApplication.cs
+++ GameApplication.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 public class GameApplication : MonoBehaviour
 {
+    /*快捷鍵
+        a => 跳到第二幕
+        b => check the value of Gameapplication.scene
+    */
    
    static GameApplication instance;
    private void Awake()
@@ -21,6 +25,8 @@ public class GameApplication : MonoBehaviour
        if(PresentScene == "scene0") scene = "0";
        if(PresentScene == "scene1") scene = "1";
        if(PresentScene == "scene2") scene = "2";*/
+       if(Input.GetKeyDown("a")) OnStartGame("scene2");
+       if(Input.GetKeyDown("b")) Debug.Log(scene);
    }
    public void OnStartGame(string SceneName){
         UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
---
+++ LeafCreator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class LeafCreater : MonoBehaviour
+
+{
+
+    //要被生成的怪物物件
+    public GameObject leaf;
+
+    void Start()
+
+    {
+
+        //執行生成怪物程式碼(每秒一次)
+
+        InvokeRepeating("CreateLeaf", 1, 1);
+
+    }
+
+    public void CreateLeaf()
+
+    {
+
+        int leafNum;
+
+        //隨機決定要生成幾個怪物(0-2個隨機)
+
+        leafNum = Random.Range(0, 3);
+
+        //開始生成怪物
+
+        for (int i = 0; i < leafNum; i++)
+
+        {
+
+            //宣告生成的X座標
+
+            float x;
+
+            //產生隨機的X座標(-6到5之間)
+
+            x = Random.Range(-6, 6);
+
+            //生成怪物
+
+            Instantiate(leaf, new Vector3(x, 2.8f, 0), Quaternion.identity);
+
+        }
+
+    }
+
+}
